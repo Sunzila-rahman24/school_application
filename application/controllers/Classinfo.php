9855b1d6fdfacd6d19eb3e9a6a6ce163df846e30
nayembabu <babunayem@gmail.com>
2020-10-17 18:38:22 +0600

    admin mastering completed

---
+++ application/controllers/Classinfo.php
@@ -0,0 +1,17 @@
+<?php 
+
+class Classinfo extends CI_Controller
+{
+    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $this->load->view('class_section/class_info');        
+    }
+    
+}
+
