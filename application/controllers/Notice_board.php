649e376aad2ec6bc1650ff959263a320ad99b357
Masud <masudapon4@gmail.com>
2020-10-18 16:12:51 +0600

    Noticeboard

--- application/controllers/Notice_board.php
+++ application/controllers/Notice_board.php
@@ -1,14 +1,22 @@
-<?php
+<?php 
 
 class Notice_board extends CI_Controller
 {
-	
-	public function __construct()
-	{
-		parent ::__construct();
-	}
-	public function index()
-	{
-		$this->load->view('noticeboard/noticeboard');
-	}
+    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+    	
+    	$this->load->view('admin_master/head');
+        $this->load->view('noticeboard/noticeboard');
+        $this->load->view('admin_master/foot');
+
+                
+    }
+    
 }
+
